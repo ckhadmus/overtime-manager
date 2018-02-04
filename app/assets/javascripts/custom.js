90c152dac6160467a14ec2a5c8a95091fa9a2b9d
Carlos Orellana <c.marroquin2106@gmail.com>
2018-02-04 15:39:15 -0600

    Added better styles to alert messages

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   setTimeout(function() {
-    $('.notice_wrapper, .alert_wrapper, .alert_error_wrapper').fadeOut('slow', function(){
+    $('.notice_wrapper, .alert_wrapper, .alert_error_wrapper, .validate-errors-alert').fadeOut('slow', function(){
       $(this).remove();
     });
   }, 2500);
